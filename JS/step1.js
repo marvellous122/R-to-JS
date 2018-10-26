2f15756e243f9e0d270cf247ee4694b6834d9db1
marvellous122 <marvellouspeak@gmail.com>
2018-10-26 15:08:58 -0400

    merging

--- JS/step1.js
+++ JS/step1.js
@@ -66,7 +66,14 @@ export const any = function () {
 
 export const isNull = function () {
   if (arguments.length === 1) {
-    if (arguments[0] === null) {
+    if (Array.isArray(arguments[0])) {
+      for (i = 0; i < arguments[0].length; i++) {
+        if (arguments[0][i] !== null) {
+          return false;
+        }
+      }
+      return true;
+    } else if(arguments[0] === null) {
       return true;
     }
 
@@ -90,7 +97,7 @@ export const isFunction = function () {
 
 export const isFinite = function () {
   if (arguments.length === 1) {
-    if (arguments[0] instanceof Array) {
+    if (Array.isArray(arguments[0])) {
       return arguments[0].map(arg => isFinite(arg));
     } else {
       return isFinite(arguments[0]);
@@ -102,7 +109,7 @@ export const isFinite = function () {
 
 export const isInfinite = function () {
   if (arguments.length === 1) {
-    if (arguments[0] instanceof Array) {
+    if (Array.isArray(arguments[0])) {
       return arguments[0].map(arg => arg === Infinity);
     } else {
       return arguments[0] === Infinity;
@@ -137,4 +144,119 @@ export const stop = function () {
     throw new Error `in ${arguments[0].toString()}`;
   }
   throw new Error;
+}
+
+export const nzchar = function () {
+  let arr = []
+  const _nzchar = function () {
+    // let NA=undefined
+    let arg = arguments
+    if (arg[1] === undefined) arg[1] = false
+    if (arg.length === 0) return "Error, 0 arguments passed to 'nzchar' which requires 1 to 2"
+    if (arg[2]) return "Error, more than 1 arguments passed to 'nzchar' which requires 1 to 2"
+    if ((arg[0] instanceof Object && (arg[0].length === undefined || arg[0].length === 0)) || arg[0] === null) return 'logical(0)'
+    if (arg[0] === undefined && arg[1] == true) { arr.push (undefined) }
+    if (arg[1] == false) {
+      if (arg[0] !== '') arr.push (true)
+      else arr.push(false)
+    }
+    if (Array.isArray(arg[0]) && arg[0].length > 0) {
+      for (let i = 1; i < arg[0].length; i++) {
+        _nzchar(arg[0][i])
+      }
+    }
+    return arr
+  }
+  return _nzchar(...arguments)
+}
+
+//* #######################################
+//* returns TRUE if its argument's value is NULL and FALSE otherwise.
+export const isNull = function () {
+  let arr = [];
+  const _isNull = function () {
+    let arg = arguments;
+    if (arg.length === 0) {
+      throw "Error, 0 arguments passed to 'isNull' which requires 1 to 2";
+    }
+    if (arg[1]) {
+      throw "Error! more than 1 arguments passed to 'isNull' which requires 1";
+    }
+    if (arg[0] == null || (arg[0] instanceof Object && (arg[0].length === undefined || arg[0].length === 0))) {
+      arr.push(true);
+    } else {
+      arr.push(false);
+    }
+    if (Array.isArray(arg[0]) && arg[0].length > 0) {
+      for (let i = 1; i < arg[0].length; i++) {
+        _isNull(arg[0][i])
+      }
+    }
+    return arr
+  }
+  return _isNull(...arguments)
+}
+
+export const isNa = function () {
+  let arr = []
+  const _isNa = function () {
+    let arg = arguments
+    if (arg.length === 0) {
+      throw "Error, 0 arguments passed to 'isNa' which requires 1 to 2";
+    }
+    if (arg[1]) {
+      throw "Error! more than 1 arguments passed to 'isNa' which requires 1";
+    }
+    if (arg[0] == null || (arg[0] instanceof Object && (arg[0].length === undefined || arg[0].length === 0))) {
+      return 'logical(0)';
+    }
+    if (arg[0] === undefined) {
+      arr.push(true);
+    } else {
+      arr.push(false);
+    }
+    if (Array.isArray(arg[0]) && arg[0].length > 0) {
+      for (let i = 1; i < arg[0].length; i++) {
+        _isNa(arg[0][i]);
+      }
+    }
+    return arr;
+  }
+  return _isNa(...arguments);
+}
+
+export const isNumeric = function () {
+  let lg = true;
+  if (arguments.length === 0) {
+    throw "Error, 0 arguments passed to 'isNumeric' which requires 1 to 2";
+  }
+  if (arguments[1]) {
+    throw "Error! more than 1 arguments passed to 'isNumeric' which requires 1";
+  }
+  if (arguments[0] === null || arguments[0] === undefined) {
+    return false;
+  }
+  if (arguments[0] instanceof Object && (arguments[0].length === undefined || arguments[0].length === 0)) {
+    return false;
+  } else if (Array.isArray(arguments[0]) && arguments[0].length > 0) {
+    for (let i = 1; i < arguments[0].length; i++) {
+      lg = (typeof arguments[0][i] === 'number') && lg
+    }
+  }
+  return lg
+}
+
+export const asLogical = function (value = 'Empty') {
+  if (value === null || value === 'Empty') return 'logical(0)'
+  if (isNaN(value) || value === undefined) return undefined
+  if (Number.isInteger(value) !== true || value === 'F' || value === 'FALSE' || value === 'False') return false
+  if (Number.isInteger(value) === true || value === 'T' || value === 'TRUE' || value === 'True') return true
+}
+
+export const seqLen = function (value = 'Empty') {
+  if (value === 'Empty' || value === 0) return 'logical(0)'
+  if (isNaN(value) || value === undefined || value === null || value < 0) return 'Error: argument must be coercible to non-negative integer'
+  else {
+    return Array.from(new Array(Math.round(value)), (val, index) => index + 1)
+  }
 }
